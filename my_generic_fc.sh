953a4604fe95cecac2233d84ebe55b1c11d337c6
javi3code <j.code.initialize@gmail.com>
2023-05-13 13:12:15 +0200

    [Init]: Initialize Repository

---
+++ my_generic_fc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Title: Useful functions for semi-regular use
+# Description: A collection of useful functions for semi-regular use
+# Author: Javi3Code
+# Date: Saturday May 13, 2023
+#
