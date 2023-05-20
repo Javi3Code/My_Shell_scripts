a1afdf8d341f54512e585bd6225235d5e209e97b
javi3code <j.code.initialize@gmail.com>
2023-05-20 13:49:37 +0200

    Add new branch-fc

--- git_utils.sh
+++ git_utils.sh
@@ -20,15 +20,48 @@
 
 ¡setupstream_branch() {
     local branch=$(__actual_branch__)
+    ¡setupstream_branch
     git branch --set-upstream-to=origin/$branch $branch
-    echo "Set upstream to origin/$branch"
+    echo "Set upstream(all) to origin/$branch"
 }
 
 ¡setupstream_push() {
     local branch=$(__actual_branch__)
     git push -u origin $branch
-    ¡setupstream_branch
-    echo "Set upstream to origin/$branch"
+    echo "Set upstream(push) to origin/$branch"
+}
+
+¡delbranch() {
+    while getopts "b:r:l" opt; do
+        case ${opt} in
+        r)
+            git push origin $OPTARG -d
+            echo "Remote $OPTARG branch has been deleted"
+            ;;
+        l)
+            git push $OPTARG -D
+            echo "Local $OPTARG branch has been deleted"
+            ;;
+        esac
+    done
+}
+
+¡backupbranch() {
+    local backup="$(__actual_branch__)-backup"
+    if [ $1 == -sw ]; then
+        git checkout -b $backup
+    else
+        git branch $backup
+    fi
+    echo "$backup has been created"
+}
+
+¡orphanbranch() {
+    git checkout --orphan $1
+    if [ $2 == -r ]; then
+        git rm -r --cached
+    fi
+    echo "$1 has been created"
 }
 
 __actual_branch__() {
